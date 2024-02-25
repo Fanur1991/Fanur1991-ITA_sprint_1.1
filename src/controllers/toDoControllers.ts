254f1ed9f7da3f9dc3d57fc8b7161eb555f02408
Fanur <fanur1991@gmail.com>
2024-02-25 10:54:31 +0300

    Update controllers and routers

--- src/controllers/toDoControllers.ts
+++ src/controllers/toDoControllers.ts
@@ -5,21 +5,64 @@ import { ToDoList, ToDo } from '../models/ToDo';
 const toDolist = new ToDoList();
 const isCompleted = false;
 
+// get todo controller
 export const getToDosList = (_req: Request, res: Response) => {
   const toDos = toDolist.getToDos();
 
   return res.json(toDos);
 };
 
+// create new todo controller
 export const createToDo = (req: Request, res: Response) => {
   const { title } = req.body;
 
   if (title === '') {
-    return res.json('Write a task');
+    return res.json({ message: 'Write a task' });
   }
 
   const newToDo = new ToDo(uuidv4(), title, isCompleted);
   toDolist.addToDo(newToDo);
 
   return res.json(newToDo);
+};
+
+// update todo title controller
+export const updateToDo = (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  toDolist.updateToDo(id, title);
+  const toDos = toDolist.getToDos();
+
+  return res.json(toDos);
+};
+
+// update todo state controller
+export const updateToDoState = (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  toDolist.changeToDoState(id);
+  const toDos = toDolist.getToDos();
+
+  return res.json(toDos);
+};
+
+// remove a todo controller
+export const removeToDo = (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.json({ message: 'That todo does not exist' });
+  }
+
+  toDolist.removeToDo(id);
+
+  return res.json({ message: 'Todo had been removed' });
+};
+
+// remove all todos controller
+export const removeToDos = (_req: Request, res: Response) => {
+  toDolist.removeAllToDos();
+
+  return res.json({ message: 'All todos had been removed' });
 };
--- src/routes/toDoRoutes.ts
+++ src/routes/toDoRoutes.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
-import { getToDosList, createToDo } from '../controllers/toDoControllers';
+import {
+  getToDosList,
+  createToDo,
+  removeToDo,
+  removeToDos,
+  updateToDo,
+  updateToDoState,
+} from '../controllers/toDoControllers';
 
 const router = Router();
 
-router.get('/', getToDosList);
-router.post('/', createToDo);
+router.get('/todos', getToDosList);
+router.post('/todo', createToDo);
+router.put('/todos/update/:id', updateToDo);
+router.patch('/todos/update/:id', updateToDoState);
+router.delete('/todos/remove/:id', removeToDo);
+router.delete('/todos/remove-all', removeToDos);
 
 export default router;
