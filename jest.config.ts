e9925c2fde6e0212b71651d48f9a4f2b55df2e57
Fanur <fanur1991@gmail.com>
2024-02-29 08:29:46 +0300

    update config? package.json

--- jest.config.ts
+++ jest.config.ts
@@ -181,7 +181,7 @@ const config: Config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ["/client/"],
+  testPathIgnorePatterns: ['/client/'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
